694c6b28bb46bb47405e101979fc4299bd31a076
Rafael Ruiz <rafael@macbook.com>
2015-03-29 16:42:40 +0200

    Add RGB pattern check

--- js/sketch.js
+++ js/sketch.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-  alert("Debug: Start JS");
   initSketch();
   paintOnHover();
 
@@ -43,31 +42,63 @@ function initSketch(grid_value){
 function resetSketch(option) {
   var grid_value = prompt("Enter the number of squares per row", "16");
   initSketch(grid_value);
-  if(option == "trail") {
-    trailOnHover();
-  }
-  else {
-    paintOnHover();
+  switch(option) {
+    case 'trail':
+      trailOnHover();
+      break;
+    case 'gradient':
+      gradientOnHover();
+      break;
+    default:
+      paintOnHover();
+      break;
   }
 }
 
 function paintOnHover() {
-  $(' .square ').hover( function() {
+  $(' .square ').mouseenter( function() {
+    $(this).addClass("highlight");
     var $status = $(' .status ');
     if($status.hasClass("active")) {
       $( this ).addClass("coloured");
     }
   });
+
+  $(' .square ').mouseleave( function() { 
+    $(this).removeClass("highlight");
+  }); 
 }
 
-// function paintOnHover() {
-//   $(".square").hover( 
-//     function() {
-//     if($('.status').hasClass("active")) {
-//       $(this).addClass("coloured");
-//     }
-//   );
-// }
+function gradientOnHover() {
+  $(' .square ').mouseenter( function() {
+    $(this).addClass("highlight");
+
+    var $status = $(' .status ');
+    if($status.hasClass("active")) {
+      if($(this).hasClass("gradient")){
+        var colour = $(this).css('background-color');
+        var rgb_pattern = /rgb\(\d{1,3},\s?\d{1,3},\s?\d{1,3}\)/
+        if(rgb_pattern.test(colour)){
+          alert("RGB pattern!");
+        }
+        console.log("Colour: " + colour);
+        var index_of_opacity = colour.lastIndexOf(',') + 1;
+        var current_opacity = colour.slice(index_of_opacity, -1);
+        var new_opacity = parseFloat(current_opacity) + 0.1;
+        var new_colour = colour.substring(0,index_of_opacity) + ' ' + new_opacity + ')';
+        console.log("New Colour: " + new_colour);
+        $(this).css('background-color', new_colour);  
+      }
+      else {
+        $( this ).addClass("gradient"); 
+      }
+    }
+  });
+
+  $(' .square ').mouseleave( function() { 
+    $(this).removeClass("highlight");
+  }); 
+}
 
 function trailOnHover() {
   $('.square').hover(
